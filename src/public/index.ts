a60082e7124b7ea1c268b18ba711c15c2249a71f
user <chris.amshey@gmail.com>
2023-01-18 12:25:58 -0500

    Basic gameplay loop working

--- src/public/index.ts
+++ src/public/index.ts
@@ -1,42 +1,296 @@
 "use strict"
 
-
 document.body.innerHTML = "what"
 
 type Dir = 'left' | 'right' | 'up' | 'down';
+type Cmd = Dir | 'rotate';
+type MapCodeToDir = {
+    [key: string] : Cmd
+}
 
-const codeToDir : any = {
+const codeToDir : MapCodeToDir = {
     'ArrowLeft' : 'left',
     'ArrowRight' : 'right',
-    'ArrowUp' : 'up',
+    'ArrowUp' : 'rotate',
     'ArrowDown' : 'down',
-    'KeyW' : 'up',
+    'KeyW' : 'rotate',
     'KeyS' : 'down',
     'KeyA' : 'left',
     'KeyD' : 'right'
+}
+
+const cellValue = [ '.', '@', '#', '*' ] as const;
+type CellValue = typeof cellValue[number];
+//   ^?
+
+const pieceTable : CellValue[][][] = [
+    [ 
+        [ '.', '.', '.' ],
+        [ '#', '#', '#' ],
+        [ '.', '#', '.' ],
+    ],
+
+    [ 
+        [ '#', '#' ],
+        [ '#', '#' ],
+    ],
+    [ 
+        [ '.', '.', '.' ],
+        [ '#', '.', '.' ],
+        [ '#', '#', '#' ],
+    ],
+    [ 
+        [ '.', '.', '.' ],
+        [ '.', '.', '#' ],
+        [ '#', '#', '#' ],
+    ],
+    [ 
+        [ '.', '.', '.' ],
+        [ '#', '#', '.' ],
+        [ '.', '#', '#' ],
+    ],
+
+    [ 
+        [ '.', '.', '.' ],
+        [ '.', '#', '#' ],
+        [ '#', '#', '.' ],
+    ],
+
+
+]
+
+type Coord = { row: number, col: number }
+
+class Piece {
+    cells : CellValue[][];
+    size : number;
+    bottom : number;
+    pos: Coord;
+
+    constructor()
+    {
+        const rnum = Math.floor(Math.random() * pieceTable.length);
+        
+        this.cells = pieceTable[rnum];
+        this.pos = { row: -3, col: 3 };
+        this.size = this.cells.length;
+        this.bottom = this.cells.length - 1;
+
+    }
+}
+
+class Field {
+    cells : CellValue[][];
+    pieceObj! : Piece;
+    interval : any;
+    deadrows : number;
+
+    constructor()
+    {
+        this.cells = newfield();
+        this.deadrows = 0;
+        this.newpiece();
+        this.interval = setInterval(this.tick.bind(this), 250);
+    }
+
+    newpiece()
+    {
+        this.pieceObj = new Piece();
+    }
+
+    rotate()
+    {
+        const piece = this.pieceObj.cells;
+        const newpiece : CellValue[][] = piece.map(() => []);
+        const end = newpiece.length - 1;
+
+        for(let row = 0 ; row < piece.length ; row++ ) {
+            for(let col = 0 ; col < piece[row].length ; col++) {
+                newpiece[col][end - row] = piece[row][col];
+            }
+        }
+
+        this.pieceObj.cells = newpiece;
+    }
+
+
+    outofbounds()
+    {
+        const { size, pos } = this.pieceObj;
+        
+
+        for(let row = 0 ; row < size ; row++) {
+            for(let col = 0 ; col < size ; col++) {
+                if(this.pieceObj.cells[row][col] === '#') {
+                    if(col + pos.col < 0 || col + pos.col > 9) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    ispiece(row : number, col : number)
+    {
+        const { cells, pos } = this.pieceObj;
+        if(typeof cells[row - pos.row] !== 'undefined' &&
+        cells[row - pos.row][col - pos.col] === '#') {
+            return true;
+        }
+
+    }
+
+    collision(testpos : { row: number, col: number })
+    {
+        const { size, cells } = this.pieceObj;
+        for(let row = 0 ; row < size ; row++) {
+            for(let col = 0 ; col < size ; col++) {
+                if(cells[row][col] === '#') {
+                    if(col + testpos.col < 0 || col + testpos.col > 9) {
+                        return true;
+                    }
+                    if(row + testpos.row >= this.cells.length) {
+                        return true;
+                    }
+                    if(typeof this.cells[testpos.row + row] !== 'undefined' &&
+                    this.cells[testpos.row + row][testpos.col + col] !== '.') {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    draw()
+    {
+        let f = this.toString();
+        document.body.innerHTML = `<pre>${f}</pre>`;
+        console.log("drawing...")
+    }
+    crystallize()
+    {
+        const { size, pos, cells } = this.pieceObj;
+        for(let r = 0 ; r < size ; r++) {
+            for(let c = 0 ; c < size ; c++) {
+                if(cells[r][c] === '#') {
+                    if(r + pos.row < 0) {
+                        return false;
+                    }
+                    this.cells[r + pos.row][c + pos.col] = '@';
+                }
+            }
+        }
+        this.newpiece();
+
+        return true;
+
+    }
+
+    clearrows()
+    {
+        for(let row = field.cells.length - 1 ; row >=0 ; row--) {
+            if(this.cells[row].filter( x => x === '@').length === this.cells[row].length) {
+                this.cells[row] = ''.padEnd(this.cells[row].length, '*').split('') as CellValue[];
+                this.deadrows++;
+            }
+        }
+
+    }
+
+    removerows()
+    {
+        for(let i = 0 ; i < this.deadrows ; i++) {
+            for(let row = field.cells.length - 1 ; row >= 0 ; row--) {
+                if(this.cells[row].filter(x => x === '*').length === this.cells[row].length) {
+                    this.collapse(row);
+                }
+            }
+        }
+        this.deadrows = 0;
+    }
+
+    collapse(from : number)
+    {
+        console.log(`Collapse from ${from}`)
+        for(let row = from ; row > 0 ; row--) {
+            this.cells[row] = this.cells[row - 1];
+        }
+        this.cells[0] = ''.padEnd(this.cells[1].length, '.').split('') as CellValue[];
+    }
+
+    tick()
+    {
+        
+        this.removerows();
+
+        let opos = { row: this.pieceObj.pos.row, col: this.pieceObj.pos.col };
+
+        this.move('down');
+
+        let { pos } = this.pieceObj;
+
+        if(pos.row === opos.row && pos.col === opos.col) {
+            if(!this.crystallize()) {
+                console.log("Game Over!");
+                clearInterval(this.interval);
+            } else {
+                this.clearrows();
+            };
+        }
+        this.draw();
+    }
+    toString()
+    {
+        let flat = [];
+        for(let row = 0 ; row < this.cells.length ; row++) {
+            for(let col = 0 ; col < this.cells[row].length ; col++) {
+                if(this.ispiece(row, col))
+                    flat.push('#')
+                else
+                    flat.push(this.cells[row][col]);
+            }
+            flat.push('\n');
+        }
+        return flat.join('');
+    }
+    
+    move(dir : Dir) 
+    {
+        let { row, col } = this.pieceObj.pos;
+        row += dirMap[dir].row;
+        col += dirMap[dir].col;
+    
+        console.log(`move ${dir} : from ${this.pieceObj.pos.row}, ${this.pieceObj.pos.col} to ${row}, ${col}`);
+        if(!this.collision({row: row, col: col})) {
+            console.log("move to", row, col);
+            this.pieceObj.pos.row = row;
+            this.pieceObj.pos.col = col;
+        } else {
+            console.log("abort move");
+        }
+    }
 
 }
 
 let output = "<pre>no input</pre>";
 const inputs : string[] = [];
-const field : any[] = newfield();
-const pos = { row: 0, col: 0 };
-field[pos.row][pos.col] = '@';
+const field = new Field();
 
 document.body.innerHTML = output;
 
 document.body.addEventListener("keydown", (ev) =>
 {
-    inputs.push(ev.code);
-    if(inputs.length > 5) {
-        inputs.shift();
-    }
-    let d : Dir | undefined  = codeToDir[ev.code] as Dir | undefined;
+    let d : Cmd | undefined  = codeToDir[ev.code] as Cmd | undefined;
     if(typeof d !== 'undefined') {
-        move(d);
+        if(d === 'rotate') {
+            field.rotate();
+        } else {
+            field.move(d);
+        }
     }
-    let fieldstr = fieldtostring();
-    document.body.innerHTML = `<pre>${fieldstr}</pre>`;
+    field.draw();
 } );
 
 const dirMap = {
@@ -46,32 +300,14 @@ const dirMap = {
     'down' : { row: 1, col: 0 },
 }
 
-function move(dir : Dir) 
-{
-    let { row, col } = pos;
-    row += dirMap[dir].row;
-    col += dirMap[dir].col;
 
-    console.log(`move ${dir} : from ${pos.row}, ${pos.col} to ${row}, ${col}`);
-    if(row >= 0 && col >= 0 && row < 10 && col < 10) {
-        console.log("move to", row, col);
-        field[pos.row][pos.col] = '.';
-        field[row][col] = '@';
-        pos.row = row;
-        pos.col = col;
-    }
-}
 
-function fieldtostring()
-{
-    return field.map((row) => row.join('')).join('\n');
-}
 
-function newfield()
+function newfield() : CellValue[][]
 {
     const field = [];
-    for(let i = 0 ; i < 10 ; i++) {
-        field[i] = ''.padEnd(10, '.').split('');
+    for(let i = 0 ; i < 20 ; i++) {
+        field[i] = ''.padEnd(10, '.').split('') as CellValue[];
     }
     return field;
 }
