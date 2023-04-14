16d4bd5d3bc44880fd328caa9ab470c1b1deaa32
user <chris.amshey@gmail.com>
2023-04-14 10:53:31 -0400

    Log port discovery

--- src/server/server.ts
+++ src/server/server.ts
@@ -6,6 +6,12 @@ import path from 'path';
 const app = express();
 const port = Number(process.env.PORT) || 80;
 
+if(port !== 80) {
+    console.log("Found environment PORT:", port);
+} else {
+    console.log("Using default port");
+}
+
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
